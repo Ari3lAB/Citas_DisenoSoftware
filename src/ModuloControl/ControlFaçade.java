6ef9670474b371836b1216dca8038e88d71079db
Ari3lAB <36025299+Ari3lAB@users.noreply.github.com>
2018-04-15 13:07:14 -0700

    Correcciones y cascaron Frame

---
+++ src/ModuloControl/ControlFaçade.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuloControl;
+
+import interfaces.IControl;
+
+/**
+ *
+ * @author Ariel AB
+ */
+public class ControlFaçade implements IControl {
+    
+}
