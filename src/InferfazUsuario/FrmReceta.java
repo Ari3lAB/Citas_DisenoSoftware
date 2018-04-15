cfbde87a52ecf34515de07cc2c02a61dd0b95128
Ari3lAB <36025299+Ari3lAB@users.noreply.github.com>
2018-04-15 15:29:56 -0700

    Frame principal y DlgBuscarPaciente

--- src/InferfazUsuario/FrmReceta.java
+++ src/InferfazUsuario/FrmReceta.java
@@ -1,5 +1,8 @@
 package InferfazUsuario;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 /**
  *
  * @author Ariiel AB
@@ -11,6 +14,28 @@ public class FrmReceta extends javax.swing.JFrame {
      */
     public FrmReceta() {
         initComponents();
+        centraVentana();
+    }
+
+    private void centraVentana() {
+        //Obtiene el tamaño de la pantalla
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Obtiene el tamaño de la ventana de la aplicación
+        Dimension frameSize = getSize();
+
+        // Se asegura que el tamaño de la ventana de la aplicación
+        // no exceda el tamaño de la pantalla
+        if (frameSize.height > screenSize.height) {
+            frameSize.height = screenSize.height;
+        }
+        if (frameSize.width > screenSize.width) {
+            frameSize.width = screenSize.width;
+        }
+
+        // Centra la ventana de la aplicación sobre la pantalla
+        setLocation((screenSize.width - frameSize.width) / 2,
+                (screenSize.height - frameSize.height) / 2);
     }
 
     /**
@@ -27,16 +52,24 @@ public class FrmReceta extends javax.swing.JFrame {
         fileMenu = new javax.swing.JMenu();
         consultaMenuItem = new javax.swing.JMenuItem();
         exitMenuItem = new javax.swing.JMenuItem();
-        helpMenu = new javax.swing.JMenu();
-        aboutMenuItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         fileMenu.setMnemonic('f');
-        fileMenu.setText("File");
+        fileMenu.setText("Consulta");
 
         consultaMenuItem.setMnemonic('o');
         consultaMenuItem.setText("Nueva consulta");
+        consultaMenuItem.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                consultaMenuItemMouseClicked(evt);
+            }
+        });
+        consultaMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                consultaMenuItemActionPerformed(evt);
+            }
+        });
         fileMenu.add(consultaMenuItem);
 
         exitMenuItem.setMnemonic('x');
@@ -49,15 +82,6 @@ public class FrmReceta extends javax.swing.JFrame {
         fileMenu.add(exitMenuItem);
 
         menuBar.add(fileMenu);
-
-        helpMenu.setMnemonic('h');
-        helpMenu.setText("Help");
-
-        aboutMenuItem.setMnemonic('a');
-        aboutMenuItem.setText("About");
-        helpMenu.add(aboutMenuItem);
-
-        menuBar.add(helpMenu);
 
         setJMenuBar(menuBar);
 
@@ -78,6 +102,15 @@ public class FrmReceta extends javax.swing.JFrame {
     private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
         System.exit(0);
     }//GEN-LAST:event_exitMenuItemActionPerformed
+
+    private void consultaMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_consultaMenuItemActionPerformed
+        DlgBuscarPaciente dialog = new DlgBuscarPaciente(this, "Buscar Paciente");
+
+        dialog.setVisible(true);    }//GEN-LAST:event_consultaMenuItemActionPerformed
+
+    private void consultaMenuItemMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_consultaMenuItemMouseClicked
+
+    }//GEN-LAST:event_consultaMenuItemMouseClicked
 
     /**
      * @param args the command line arguments
@@ -116,12 +149,10 @@ public class FrmReceta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuItem aboutMenuItem;
     private javax.swing.JMenuItem consultaMenuItem;
     private javax.swing.JDesktopPane desktopPane;
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
-    private javax.swing.JMenu helpMenu;
     private javax.swing.JMenuBar menuBar;
     // End of variables declaration//GEN-END:variables
 
